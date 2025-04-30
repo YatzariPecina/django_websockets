a3be7da776956ffcce43fa31caf07f192560ba7b
YatzariPecina <2130039@upv.edu.com>
2025-04-30 09:30:07 -0600

    allowed hosts modified

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$e4$ult&dsh=x!vme$zss6mmi18j2!j4gx!y_*du$=(ax_s2y)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
